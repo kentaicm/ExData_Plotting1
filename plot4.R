5130971c92dd1dfab31f31eae14abbf4397efa24
kentaicm <31612864+kentaicm@users.noreply.github.com>
2017-09-14 14:21:39 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -17,10 +17,10 @@ SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%
 finalData <- cbind(SetTime, finalData)
 
 ## Generate Plot 4
+png(filename="plot4.png")
 labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
 columnlines <- c("black","red","blue")
 par(mfrow=c(2,2))
-png(filename="plot4.png")
 plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power")
 plot(finalData$SetTime, finalData$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
 plot(finalData$SetTime, finalData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering",col="black")
